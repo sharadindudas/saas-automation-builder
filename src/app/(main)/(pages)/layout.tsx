db51e4d0576730f5482d4f346cd1dbbf90464584
Sharadindu Das <sharadindudas774@gmail.com>
2025-06-29 23:35:48 +0530

    feat: Added basic sidebar + dashboard page layout

---
+++ src/app/(main)/(pages)/layout.tsx
@@ -0,0 +1,11 @@
+export default function PagesLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="border-l-[1px] border-t-[1px] pb-20 h-screen border-muted-foreground/20 overflow-scroll">
+            {children}
+        </div>
+    );
+}
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,7 +2,7 @@ import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-const Navbar = async (props: Props) => {
+const Navbar = async () => {
     return (
         <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
             <aside className="flex items-center gap-[2px]">
