f31927a88ef7e6b98fec4da560ece535e4b5695c
Sharadindu Das <sharadindudas774@gmail.com>
2025-07-02 21:23:43 +0530

    fix: FIxed some layout issue

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,11 @@
 import Sidebar from "@/components/sidebar";
 
-export default function MainLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="flex overflow-hidden h-screen">
-      <Sidebar />
-      <div className="w-full">{children}</div>
-    </div>
-  );
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex overflow-hidden min-h-screen">
+            <Sidebar />
+            <div className="w-full">{children}</div>
+        </div>
+    );
 }
+
